9234fddc8de8ac5bd27c07043e2169852bccc5ac
tonkhao <33198781+tonkhao@users.noreply.github.com>
2025-11-09 14:41:13 +0300

    this doesnt work

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,16 +1,47 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Тестируем index.html', async ({ page }) => {
-  await page.goto('http://127.0.0.1:3000/index.html');
-  await expect(page).toHaveTitle("Home");
-});
+const pages = [
+  {
+    pageName: "Home",
+    pageUrl: "index.html",
+  },
+  {
+    pageName: "About",
+    pageUrl: "about.html",
+  },
+  {
+    pageName: "Contact",
+    pageUrl: "contact.html",
+  },
+];
 
-test('Тестируем about.html', async ({ page }) => {
-  await page.goto('http://127.0.0.1:3000/about.html');
-  await expect(page).toHaveTitle("About");
-});
+for (const [index, currentPage] in pages.entries()) {
+  test(`Тестируем ${currentPage.pageUrl} ${index}`, async ({ page }) => {
+    await page.goto(`http://127.0.0.1:3000/${pageName}.html`);
+    await expect(page).toHaveTitle(currentPage.pageName);
 
-test('Тестируем contact.html', async ({ page }) => {
-  await page.goto('http://127.0.0.1:3000/contact.html');
-  await expect(page).toHaveTitle("Contact");
-});
+    const homeLocator = await page.getByRole("link", { name: currentPage.pageName });
+    const homeHref = await homeLocator.getAttribute("href");
+    expect(homeHref).toBe(currentPage.pageUrl);
+    await homeLocator.click();
+    await expect(page).toHaveTitle(currentPage.pageName);
+
+    await page.goto(`http://127.0.0.1:3000/${currentPage.pageUrl}`);
+    await expect(page).toHaveTitle(currentPage.pageName);
+
+    const aboutLocator = await page.getByRole("link", { name: currentPage.pageName });
+    const aboutHref = await aboutLocator.getAttribute("href");
+    expect(aboutHref).toBe(currentPage.pageUrl);
+    await aboutLocator.click();
+    await expect(page).toHaveTitle(currentPage.pageName);
+
+    await page.goto(`http://127.0.0.1:3000/${currentPage.pageUrl}`);
+    await expect(page).toHaveTitle(currentPage.pageName);
+
+    const contactLocator = await page.getByRole("link", { name: currentPage.pageName });
+    const contactHref = await contactLocator.getAttribute("href");
+    expect(contactHref).toBe(currentPage.pageUrl);
+    await contactLocator.click();
+    await expect(page).toHaveTitle(currentPage.pageName);
+  });
+}
